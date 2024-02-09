94e5dbc4fa4f19db65ff5624f35a6b26f19459ac
abhijeet <abhijeet3016@gmail.com>
2024-02-09 16:24:50 +0100

    Añadido un nuevo animal

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var animals = {
     "bear": "growl",
     "frog": "croak",
     "lion": "roar",
-    "bird": "tweet"
+    "bird": "tweet",
+    "elephant": "trumpet",
+    "donkey":"bray",
 }
 
 function getAnimal() {
