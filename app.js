c5e5ddac2a08cb3987ce5a925e709036ba6a5431
Abhijeet Singh <116766794+iamabhijeet2003@users.noreply.github.com>
2024-02-09 23:58:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var animals = {
     "bear": "growl",
     "frog": "croak",
     "lion": "roar",
-    "bird": "tweet"
+    "bird": "tweet",
+    "elephant":"tromphet",
+    "donkey":"bray"
 }
 
 function getAnimal() {
